b3e86cb42e4b4ed0adfa02fa8f3a5eacada3c302
Dilschat <dilschat@yandex.ru>
2020-02-24 15:55:29 +0300

    project configured

--- bytecode-viewer/src/test/java/JavaCompilerTest.java
+++ bytecode-viewer/src/test/java/JavaCompilerTest.java
@@ -1,2 +1,28 @@
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import the.bytecode.club.bytecodeviewer.compilers.JavaCompiler;
+
+import java.io.File;
+import java.io.IOException;
+
 public class JavaCompilerTest {
+
+    @Test
+    public void compileCorrectFile() throws IOException {
+        JavaCompiler compiler = new JavaCompiler();
+        File source = new File(getClass().getResource("/TestSourceCorrect.java").getFile());
+        byte[] compiledBytes = compiler.compile(FileUtils.readFileToString(source), source.getName());
+
+        File compiledSource = new File("/TestSourceCorrect.class");
+        byte[] expectedBytes = compiler.compile(FileUtils.readFileToString(compiledSource), compiledSource.getName());
+
+        Assertions.assertArrayEquals(expectedBytes, compiledBytes);
+
+    }
+
+    @Test
+    public void compileFileWithError(){
+
+    }
 }
