a998dc3cbee6814ebc286d8c4ff33293dc8abb1a
Dilschat <dilschat@yandex.ru>
2020-02-24 21:14:16 +0300

    PrefixStringBuilder tested

---
+++ bytecode-viewer/src/test/java/FieldNodeDecompilerTest.java
@@ -0,0 +1,43 @@
+import com.sun.org.apache.xpath.internal.compiler.OpCodes;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.FieldNode;
+import the.bytecode.club.bytecodeviewer.decompilers.bytecode.FieldNodeDecompiler;
+import the.bytecode.club.bytecodeviewer.decompilers.bytecode.PrefixedStringBuilder;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class FieldNodeDecompilerTest {
+
+    @Test
+    public void decompileNonNullValueTest(){
+        PrefixedStringBuilder builder = new PrefixedStringBuilder();
+        FieldNode node = mock(FieldNode.class);
+        node.value = "String";
+        node.access = Opcodes.ACC_PUBLIC;
+        node.name = "name";
+        node.desc = Type.getDescriptor(String.class);
+
+        System.out.println();
+        String actualResult = FieldNodeDecompiler.decompile(builder, node).toString();
+        Assertions.assertEquals("public java.lang.String name = \"String\";", actualResult);
+    }
+
+    @Test
+    public void decompileNullValueTest(){
+        PrefixedStringBuilder builder = new PrefixedStringBuilder();
+        FieldNode node = mock(FieldNode.class);
+        node.value = null;
+        node.access = Opcodes.ACC_PUBLIC;
+        node.name = "name";
+        node.desc = Type.getDescriptor(String.class);
+
+        System.out.println();
+        String actualResult = FieldNodeDecompiler.decompile(builder, node).toString();
+        Assertions.assertEquals("public java.lang.String name;", actualResult);
+    }
+}
